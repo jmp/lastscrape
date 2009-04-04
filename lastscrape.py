6c195264b96ee8b614de9218e19510c53f8761d3
Jarkko Piiroinen <jarkkop@iki.fi>
2009-04-04 15:38:17 +0300

    Process the final page as well. Also, don't print tracks that have no name.

--- lastscrape.py
+++ lastscrape.py
@@ -9,7 +9,8 @@ def process_page(page):
 	soup = BeautifulSoup(page)
 	for tr in soup.find('table', 'tracklist big').findAll('tr'):
 		artist, track, timestamp = parse_track(tr)
-		print '%s\t%s\t%s' % (artist, track, timestamp)
+		if artist and track:
+			print '%s\t%s\t%s' % (artist, track, timestamp)
 
 def parse_track(tr):
 	try:
@@ -23,12 +24,12 @@ def parse_track(tr):
 		# Parsing failed
 		return (None, None, None)
 
-def scrape_data(user, request_delay=2):
+def scrape_data(user, request_delay=1):
 	num_pages = 1
 	url = 'http://last.fm/user/%s/library/recent' % user
 	page = urlopen('http://last.fm/user/%s/library/recent' % user)
 	soup = BeautifulSoup(page)
-	num_pages = int(soup.find('a', 'lastpage').contents[0])
+	num_pages = int(soup.find('a', 'lastpage').contents[0]) + 1
 
 	print 'Artist\tTrack\tTimestamp'
 	for cur_page in range(1, num_pages):
